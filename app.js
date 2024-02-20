de014c61f283a254df077499972f3ce684783f16
Savadow <ujuchimaraoke@gmail.com>
2024-02-20 13:21:09 +0100

    feat(database): setup connection

---
+++ app.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+require("express-async-errors");
+const express = require("express");
+
+const sequelize = require("./db/connect");
+
+const app = express();
+const port = 5000 || process.env.PORT;
+
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}...`);
+    });
+    await sequelize.sync();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
